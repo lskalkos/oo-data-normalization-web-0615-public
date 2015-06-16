422518f2dd5d7c41aa5720a905ba700ebe24c8e6
Lia Skalkos <hello@liaskalkos.net>
2015-06-16 13:26:47 -0400

    Complete

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,13 @@
 class Song
   # code goes here
+  attr_accessor :title, :artist
+
+  PATH = "./tmp"
+
+  def serialize
+    path = './tmp/' + self.title.downcase.gsub(/\s/, '_') + '.txt'
+    content = self.artist.name + " - " + self.title
+    File.write(path, content)
+  end
+
 end
